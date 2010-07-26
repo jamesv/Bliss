3ba48e2a82b8ae6fb988a7a2104279ccee6a3cbc
james vreeland <j@mesv.org>
2010-07-26 11:06:20 -0400

    photo gallery and fb connect

---
+++ site_media/js/core.js
@@ -0,0 +1,21 @@
+/* Facebook social bits*/
+window.fbAsyncInit = function() {
+    FB.init({appId: '105150512871756', status: true, cookie: true, xfbml: true});
+};
+
+(function() {
+    var e = document.createElement('script'); e.async = true;
+    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+    document.getElementById('fb-root').appendChild(e);
+}());
+
+/* Google Analytics */
+var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-639236-21']);
+    _gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
